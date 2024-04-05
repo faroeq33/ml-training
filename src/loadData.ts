c0b4ffda8dd6b2a08d96d719578108a1d2feccf0
Faroeq <alves_1044@hotmail.com>
2024-04-05 19:51:28 +0200

    Renaming

--- src/loadData.ts
+++ src/loadData.ts
@@ -28,12 +28,46 @@ export function getAllPoses() {
         0,
         Math.floor(flattenedData.length * 0.8)
       );
+      console.log("testing loading");
+
+      saveToLocalstorage("trainingsposes", trainingData); //localstorage
       const testingData = flattenedData.slice(
         Math.floor(flattenedData.length * 0.8) + 1
       );
+      saveToLocalstorage("trainingsposes", testingData);
       return { trainingData, testingData };
 
       // gebruik de train en test data om de nn te trainen
     })
     .then((data) => data);
+}
+
+function saveToLocalstorage(keyName = "pose-", myPoses: unknown) {
+  // localStorage.setItem(`myPoses-${datetime}`, JSON.stringify(myPoses));
+  const currentdate = new Date();
+
+  const datetime =
+    currentdate.getDate() +
+    "-" +
+    (currentdate.getMonth() + 1) +
+    "-" +
+    currentdate.getFullYear() +
+    "@" +
+    currentdate.getHours() +
+    "h" +
+    currentdate.getMinutes() +
+    "m" +
+    currentdate.getSeconds() +
+    "s";
+
+  const finalPoses = JSON.stringify({ data: myPoses }, null, 2);
+  localStorage.setItem(`${keyName}-${datetime}`, "test");
+
+  // const blob = new Blob([finalPoses], { type: "application/json" });
+  // const url = URL.createObjectURL(blob);
+  // const link = document.createElement("a");
+  // link.href = url;
+  // link.download = `poses-${datetime}.json`;
+  // link.click();
+  // URL.revokeObjectURL(url);
 }
